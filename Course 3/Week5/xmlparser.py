24f28a883ac0888dc8a620683e088dca087bdce1
Mayank <jhamayank159@gmail.com>
2020-05-15 13:20:19 +0530

    Preetify

--- Course 3/Week5/xmlparser.py
+++ Course 3/Week5/xmlparser.py
@@ -3,13 +3,14 @@
 import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
-add=0
+
 #ignore ssl issues
 ctx=ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 #code
+add=0
 while True :
     url=input("Enter URL: ")
     if len(url)<1 : break
